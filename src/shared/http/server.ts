38cc6ad2930b1459ec7c5ab074ca52d44e390124
Pedro Santos <pedrohenry.viana@gmail.com>
2021-03-12 16:13:10 -0300

    feat(apperror): add custom error handling
    creates a class that customizes the sending of errors and performs error handling through middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
 
 const app = express();
@@ -9,6 +10,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server run!');
